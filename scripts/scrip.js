be98d60ba97d9ed40159ef99a70ab1f024d080d5
juanpa300478 <juanpa300478@gmail.com>
2023-09-15 12:52:29 -0500

    se mejoro el menu para la vista movil, se creo una función que permite la apertura de los sub-menus

--- scripts/scrip.js
+++ scripts/scrip.js
@@ -6,10 +6,23 @@ function updatemenu() {
       document.getElementById('menu').style.borderRadius = '0px';
     }
   }
+  
+  const menuMove =()=>{
+  let found = true;
+  document.querySelectorAll('.dropdown-arrow').forEach(element => {
+    element.addEventListener("touchstart",(e)=>{
+      e.preventDefault();
+      console.log(element.nextElementSibling.getAttribute('style'));
+      if(element.nextElementSibling.getAttribute('style') == null){
+        document.querySelectorAll('.dropdown-arrow').forEach(ele => {ele.nextElementSibling.removeAttribute('style')})
+        element.nextElementSibling.setAttribute('style','display:block');
+      }else{
+        element.nextElementSibling.removeAttribute('style');
+      } 
+   })
+  });
+}
+menuMove();
  
-  const element = document.querySelector('.dropdown-arrow');
-    element.addEventListener('click',(e)=>{
-        e.preventDefault();
-        console.log('hola')
-     })
+    
   
