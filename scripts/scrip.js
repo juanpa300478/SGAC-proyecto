4e0300dc177185d95a9b8387a6a64ff858eb384b
juanpa300478 <juanpa300478@gmail.com>
2023-09-15 13:05:13 -0500

    mejoras a la funcion munuMonve

--- scripts/scrip.js
+++ scripts/scrip.js
@@ -6,13 +6,13 @@ function updatemenu() {
       document.getElementById('menu').style.borderRadius = '0px';
     }
   }
-  
+  //funcion para abrir el submenu y cerralo(hubiera sido mas facil utilizar classList.toggle)
+  //pero por especificidad en el menu no dejaba que utilizara la clase
   const menuMove =()=>{
   let found = true;
   document.querySelectorAll('.dropdown-arrow').forEach(element => {
     element.addEventListener("touchstart",(e)=>{
       e.preventDefault();
-      console.log(element.nextElementSibling.getAttribute('style'));
       if(element.nextElementSibling.getAttribute('style') == null){
         document.querySelectorAll('.dropdown-arrow').forEach(ele => {ele.nextElementSibling.removeAttribute('style')})
         element.nextElementSibling.setAttribute('style','display:block');
