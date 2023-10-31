bbfc75f7be7ebaa4b2cda7911afa03b16395d463
juanpa300478 <juanpa300478@gmail.com>
2023-10-31 10:32:34 -0500

    mejoras a los estilos, creación de pagina para editar perfiles, creacion de una calculadora de interes

--- scripts/scrip.js
+++ scripts/scrip.js
@@ -50,6 +50,59 @@ iconMoveClick();
 menuMove();
 iconMove();
 //autenticación
+const $app = document.getElementById('app');
+const setCalculator = () =>{
+  return `
+      <form id="showCalculator">
+        <label for="cu">Cuota</label>
+        <input type="number" id="cu" name="cu" placeholder="0">
+        <label for="me">Meses</label>
+        <input type="number" id="me" name="me" placeholder="0">
+        <label for="in">Interes Mensual</label>
+        <input type="text" id="in" name="in" placeholder="0.00">
+        <button type="submit">Calcular</button>
+        </form>`;
+}
+const result = (interest,total) =>{
+  return `
+      <form id="showResult">
+        <label>Interes</label>
+        <input type="number" disabled value="${interest}">
+        <label>valor cuota total</label>
+        <input type="number" disabled value="${total}">
+        <button type="submit">Agregar valor</button>
+        <button type="reset">volver a calcular</button>
+        </form>`;
+}
+$app.innerHTML = setCalculator();
 
-    
-  
+  let valueMonth;
+  let numberMonth;
+  let interest ;
+  let valueInterest;
+  let valueTotal;
+  let found = true;
+  document.getElementById('calculator').addEventListener('submit',(e)=>{
+    e.preventDefault();
+    if(found){
+         valueMonth = Number(e.target.cu.value);
+         numberMonth = Number(e.target.me.value);
+         interest = Number(e.target.in.value);
+         valueInterest = valueMonth * interest / 100;
+         valueTotal = valueInterest * numberMonth + valueMonth;
+        $app.innerHTML = result(valueInterest,valueTotal);
+        found = false;
+        document.getElementById('showResult').addEventListener('reset',(e)=>{
+          e.preventDefault();
+          $app.innerHTML = setCalculator();
+          found = true; 
+        });
+    }else{
+        document.getElementById('valueMonthForm').value = valueTotal;
+    }
+
+  });
+  document.getElementById("calculator").addEventListener('touchstart',()=>{
+    document.getElementById("calculator").classList.toggle('calculator-open');
+  });
+
